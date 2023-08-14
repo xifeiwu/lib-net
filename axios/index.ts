1a390ab46b99df46b6001911550c71edb2d96e1a
xifei wu <xfwu@conviva.com>
2023-08-14 18:51:54 +0800

    update prettyConsoleAxiosError

--- axios/index.ts
+++ axios/index.ts
@@ -2,13 +2,23 @@ import http from 'http';
 import {logWithColor} from '../node';
 import {AxiosError} from 'axios';
 
-export function prettyConsoleAxiosError(err: AxiosError | Error) {
-  if (!(err as AxiosError).isAxiosError) {
-    return;
+export function prettyConsoleAxiosError(err: AxiosError | Error, v1: boolean = true) {
+  if (v1 && !(err as AxiosError).isAxiosError) {
+    throw err;
   }
   const {config, message, code, stack, request, response} = err as AxiosError;
   logWithColor('red', message, code);
-  if (config.maxRedirects === 0) {
+  if (v1) {
+    if (config.maxRedirects === 0) {
+      const {data} = config;
+      if (request) {
+        const {method, host, path, protocol} = request;
+        logWithColor('black', `${method} ${protocol}//${host}${path}`, {...request.getHeaders()}, data);
+      }
+    } else {
+      throw err;
+    }
+  } else {
     const {data} = config;
     if (request) {
       const {method, host, path, protocol} = request;
