0ee8eb078fc53c036058f9f79f2eb269db77a5cf
xifei wu <xfwu@conviva.com>
2024-05-13 15:04:35 +0800

    more external functions

--- external.ts
+++ external.ts
@@ -35,8 +35,11 @@ export {
   handleSocketEvents,
   parseHttpHeaderPart,
   startSocketServer,
+<<<<<<< HEAD
   requestAndGetUpgradeInfo,
   requestAndGetRelatedInfo,
   httpRequestOptionsToCurlCommand,
+=======
+>>>>>>> 4c3f697 (more external functions)
   toBuffer,
 } from '../node';
