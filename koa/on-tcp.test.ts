97de3530a5c8de671c91032b867dad447981f268
xifei wu <xfwu@conviva.com>
2024-06-19 14:50:05 +0800

    more test cases for on-tcp

--- koa/on-tcp.test.ts
+++ koa/on-tcp.test.ts
@@ -1,12 +1,50 @@
 import Koa from 'koa';
 import assert from 'assert';
-import {
-  handleSocketEvents,
-} from '../external';
+import {Socket} from 'net';
+import {handleSocketEvents} from '../external';
 import {startTcpServer} from './on-tcp';
-import {requestAndGetResponseInfo, startSocketClient} from '../../node';
+import {requestAndGetResponseInfo, startSocketClient, waitFor} from '../../node';
 
-export async function testLogic() {
+async function echoData(firstChunk: Buffer, socket: Socket) {
+  const onData = chunk => {
+    socket.write(chunk);
+  };
+  onData(firstChunk);
+  socket.on('data', onData);
+}
+async function uppercaseData(firstChunk: Buffer, socket: Socket) {
+  const onData = (chunk: Buffer) => {
+    socket.pause();
+    socket.write(chunk.toString().toUpperCase());
+    socket.resume();
+  };
+  onData(firstChunk);
+  socket.on('data', onData);
+}
+
+const dataSendOverTcp = ['write data1', 'end data2'];
+const invalidHttp = [['get /api http/1.1', 'line1', 'line2'].join('\r\n')];
+// const invalidHttp = [['set abc 0 500000 2', 'dd'].join('\r\n')];
+const handlerCaseList = {
+  echoData: {
+    handler: echoData,
+    dataSend: dataSendOverTcp,
+    dataReply: dataSendOverTcp.join(''),
+  },
+  echoInvalidHttp: {
+    handler: echoData,
+    dataSend: invalidHttp,
+    dataReply: invalidHttp.join(''),
+  },
+  uppercaseData: {
+    handler: uppercaseData,
+    dataSend: dataSendOverTcp,
+    dataReply: dataSendOverTcp.join('').toUpperCase(),
+  },
+};
+const caseName = 'echoInvalidHttp';
+
+export async function generalTest() {
   const httpBlankBody = 'blank';
   let connectionCnt = 0;
 
@@ -24,6 +62,8 @@ export async function testLogic() {
     ctx.status = 404;
     ctx.body = httpBlankBody;
   });
+  const {handler, dataSend, dataReply} = handlerCaseList[caseName];
+
   const {host, port, server} = await startTcpServer({
     onConnection: async socket => {
       connectionCnt++;
@@ -33,23 +73,31 @@ export async function testLogic() {
         onData: null,
       });
     },
-    async tcpHandler(firstChunk, socket) {
-      socket.write(firstChunk);
-      socket.pipe(socket).pipe(socket);
-    },
+    tcpHandler: handler,
     koa: app,
   });
 
   const client = await startSocketClient({host, port});
-  const dataSendOverTcp = 'data sent';
-  const replyFromServer = await new Promise<string>(res => {
-    client.write(dataSendOverTcp);
-    client.end(dataSendOverTcp);
+  const replyFromServer = await new Promise<string>(async res => {
+    let cnt = 0;
+    while (cnt < dataSend.length) {
+      if (cnt === dataSend.length - 1) {
+        client.end(dataSend[cnt]);
+      } else {
+        client.write(dataSend[cnt]);
+      }
+      cnt++;
+      await waitFor(1000);
+    }
+    const bufList: Buffer[] = [];
     client.on('data', chunk => {
-      res(chunk.toString());
+      bufList.push(chunk);
+    });
+    client.on('end', () => {
+      res(Buffer.concat(bufList).toString());
     });
   });
-  assert.equal([dataSendOverTcp, dataSendOverTcp].join(''), replyFromServer);
+  assert.equal(dataReply, replyFromServer);
 
   const response1Info = await requestAndGetResponseInfo(
     {
@@ -66,4 +114,42 @@ export async function testLogic() {
   });
   assert.equal(response2Info.data, 3);
   server.close();
+}
+
+import {Store, getConnectionHandler, getClient} from '../../node/lib/memcached';
+export async function testMemcached() {
+  const store = new Store();
+  const memcachedHandler = getConnectionHandler(store);
+
+  const app = new Koa();
+  app.use(async (ctx, next) => {
+    const {url} = ctx;
+    if (url.startsWith('/api')) {
+      ctx.status = 200;
+      ctx.body = store.toJSON();
+    } else {
+      await next();
+    }
+  });
+  const {host, port, server} = await startTcpServer({
+    onConnection: async socket => {
+      handleSocketEvents(socket, {
+        maxPrintDataLength: 100,
+        color: 'red',
+        onData: null,
+      });
+    },
+    async tcpHandler(firstChunk, socket) {
+      memcachedHandler(socket, firstChunk);
+    },
+    koa: app,
+  });
+  const client = getClient({host: '127.0.0.1', port});
+  const setRes = await client.set({
+    expireTimeInSeconds: 500000,
+    flags: 'd',
+    key: 'abc',
+    value: 'dd',
+  });
+  console.log(setRes);
 }
