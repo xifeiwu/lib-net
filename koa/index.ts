ced215cab0a77951c9e5717399c1cef51d56f00e
xifei wu <xfwu@conviva.com>
2024-10-12 19:14:45 +0800

    export koa middleware

--- koa/index.ts
+++ koa/index.ts
@@ -3,3 +3,7 @@ export * from './service';
 export * from './server';
 export * from './types';
 export * from './on-tcp';
+
+import cors from './middleware/cors';
+export {getLogMiddleware} from './middleware/log';
+export {cors as corsMw};
