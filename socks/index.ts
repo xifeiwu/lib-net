430e0bb3b76fef525c427fa9da022063a3bc1fd3
xifei wu <xfwu@conviva.com>
2024-10-16 16:35:09 +0800

    remove dir socks as it's removed to lib node

--- socks/index.ts
+++
@@ -1,4 +0,0 @@
-export {runSocksServerOnSocket as runSocksServeronSocket} from './server-on-socket';
-export {runSocksServerOnHttp} from './server-on-http';
-export * from './service';
-export * from './service/types';
--- socks/service/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './server';
-export * from './client';
-export * from './http-server';
-export * from './utils';
