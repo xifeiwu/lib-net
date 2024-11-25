32b41f78d615510413fabd39950366ae1415f065
xifei wu <xfwu@conviva.com>
2024-11-25 17:44:23 +0800

    add test case for /api/debug/upload

---
+++ koa/middleware/debug/mw-request.test.ts
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import path from 'path';
+import assert from 'assert';
+import {requestAndGetResponseInfo} from '../../../service/external';
+import {startKoaServer} from '../../server';
+import {debugRouter} from './mw-request';
+export async function testUpload() {
+  const {origin, server, app} = await startKoaServer({
+    requestMiddlewares: [debugRouter.routes()],
+    bodyParserOptions: {
+      uploadDir: path.resolve(__dirname, 'uploads'),
+      wayOfHandleFile: 'cacheAndSave',
+    },
+  });
+
+  const {
+    statusCode,
+    headers,
+    data: resData,
+  } = await requestAndGetResponseInfo({
+    origin,
+    pathname: '/api/debug/upload',
+    query: {
+      a: 'b',
+    },
+    method: 'post',
+    headers: {
+      'content-type': 'octet-stream',
+      'x-file-name': 'test-case-4-debug-middleware.ts',
+    },
+    data: fs.createReadStream(__filename),
+  });
+  assert.equal(statusCode, 200);
+  // console.log(resData);
+  server.close();
+}
