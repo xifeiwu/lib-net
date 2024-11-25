34c735c7c378d1114d075cf7f328588d24573cfa
xifei wu <xfwu@conviva.com>
2024-11-25 10:10:18 +0800

    add keys for default KOA_CONFIG

--- koa/service/config.ts
+++ koa/service/config.ts
@@ -1,5 +1,4 @@
 import path from 'path';
-import KoaSession from 'koa-session';
 import {KoaConfig, KoaMiddlewareConfig} from '../types';
 import {PORT, uploadDirOnCwd} from '../../service/external';
 import {SOCKS_SERVER_CONFIG} from '../middleware/socks/service';
@@ -12,6 +11,7 @@ export const mwConfigDefault: KoaMiddlewareConfig = {
 };
 
 export const KOA_CONFIG: KoaConfig = {
+  keys: ['secret local'],
   port: PORT.fullFeatureHttpServer.port,
   bodyParserOptions: {
     uploadDir: uploadDirOnCwd,
@@ -35,23 +35,4 @@ export const KOA_CONFIG: KoaConfig = {
     socksConfig: SOCKS_SERVER_CONFIG,
   },
   printOrigin: true,
-};
-
-// export const defaultConfig: Partial<KoaSession.opts> = {
-export const defaultConfig: Partial<KoaSession.opts> = {
-  key: 'koa.sess' /** (string) cookie key (default is koa.sess) */,
-  /** (number || 'session') maxAge in ms (default is 1 days) */
-  /** 'session' will result in a cookie that expires when session/browser is closed */
-  /** Warning: If a session cookie is stolen, this cookie will never expire */
-  maxAge: 86400000,
-  autoCommit: true /** (boolean) automatically commit headers (default true) */,
-  overwrite: true /** (boolean) can overwrite or not (default true) */,
-  httpOnly: true /** (boolean) httpOnly or not (default true) */,
-  signed: true /** (boolean) signed or not (default true) */,
-  rolling:
-    false /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */,
-  renew:
-    false /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/,
-  // secure: true /** (boolean) secure cookie*/,
-  sameSite: null /** (string) session cookie sameSite options (default null, don't set it) */,
 };
