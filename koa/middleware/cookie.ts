8350e4c3c7fb711e36d38b82f94cbec0f67530c2
xifei wu <xfwu@conviva.com>
2024-12-02 10:57:06 +0800

    add tmp code for cookie middleware

--- koa/middleware/cookie.ts
+++ koa/middleware/cookie.ts
@@ -10,3 +10,21 @@ cookieRouter.get('/list', async (ctx, next) => {
   const cookies = new Cookies(ctx.req, ctx.res);
   ctx.body = cookies.getAll();
 });
+
+// cookieRouter.get('/switch', async (ctx: Koa.Context) => {
+//   const {
+//     query: {sid},
+//   } = ctx;
+//   if (!sid) {
+//     throw new Error(`sid is empty`);
+//   }
+//   ctx.assert(sid, 400, 'sid is emtpy');
+//   // ctx.assert()
+//   if (Array.isArray(sid)) {
+//     throw new Error(`Shouldn't set multiple sid`);
+//   }
+//   await refreshExpireDate(sid);
+//   ctx.cookies.set(DEFAULT_SESSION_CONFIG.key, sid, {signed: true, overwrite: true});
+//   ctx.type = 'json';
+//   ctx.body = ctx.res.getHeader('set-cookie');
+// });
