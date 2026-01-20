2f947c1e90314eaf0f1030d327895a65006e2cf2
xifei wu <xfwu@conviva.com>
2026-01-20 15:16:10 +0800

    adapt for submodule rename

--- koa/middleware/mock/index.ts
+++ koa/middleware/mock/index.ts
@@ -7,7 +7,7 @@ import {
 } from '../../../service/external';
 import {getRequestBodyOfCtx} from '../../service';
 // import {MockFileFinder} from '../../node/http/mock/find';
-// import {deepEqual} from '@modules/lib/fe/common';
+// import {deepEqual} from '@modules/lib/js/common';
 // import {GeneralDataToSave} from '@modules/conviva/common/service/request-by-select-config';
 // import {IRequestConfig} from '@src/service/axios-wrapper';
 // const {mock} = serverConfig;
