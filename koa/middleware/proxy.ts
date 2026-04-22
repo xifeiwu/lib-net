5873e7922520ac1385adc99531b7e7ccbeda9232
xifei wu <xfwu@conviva.com>
2026-04-22 11:05:31 +0800

    move logic of proxy middleware to file proxy.ts

---
+++ koa/middleware/proxy.ts
@@ -0,0 +1,76 @@
+import Koa from 'koa';
+import {
+  FilterItem,
+  getPreRequestCb,
+  HttpRequestOptions,
+  isString,
+  matchFilters,
+  onRes2Proxy,
+  proxyHttpRequest,
+  ProxyStatus,
+  cacheData,
+} from '../../service/external';
+import {getRequestBodyOfCtx} from '../service';
+
+export const proxyKoaMw = (proxyConfig: {
+  /** only do proxy when request meet context filter */
+  contextFilterList?: FilterItem[];
+  /**
+   * The options should be add to requestOptions of every proxy request,
+   * it can be constant of dynamic(in format of function) value
+   */
+  globalRequestOptions?: {
+    get: () => Promise<HttpRequestOptions>;
+    maxAge?: number;
+  };
+  proxyStatusList?: ProxyStatus[];
+}) => {
+  const {contextFilterList, globalRequestOptions, proxyStatusList} = proxyConfig;
+  const {getOrFetch: getOrFetchGlobalRequestOptions} = cacheData<HttpRequestOptions>(
+    {maxAge: globalRequestOptions?.maxAge},
+    globalRequestOptions.get
+  );
+  return async (ctx: Koa.Context, next: Koa.Next) => {
+    const {path: pathname} = ctx;
+    if (!matchFilters(contextFilterList, pathname)) {
+      return await next();
+    }
+    const originData = await getRequestBodyOfCtx(ctx);
+    const globalRequestOptions = await getOrFetchGlobalRequestOptions();
+    ctx.respond = false;
+    proxyHttpRequest(ctx.req, ctx.res, {
+      originData,
+      globalRequestOptions,
+      async handleProxyRequestOptions(info) {
+        delete info?.headers['host'];
+        delete info?.headers['referer'];
+        return info;
+      },
+      preProxyReq: getPreRequestCb({
+        statusList: proxyStatusList,
+      }),
+      onRes2Proxy: onRes2Proxy,
+      async handleResponseInfoToOrigin(info) {
+        const {headers, ...restProps} = info;
+        for (let [key, value] of Object.entries(headers)) {
+          /** ignore cors related headers */
+          if (isString(key) && key.toLowerCase().startsWith('access-control-')) {
+            delete headers[key];
+          }
+        }
+        /** Add proxy info to httpResponseInfo to origin */
+        for (const [key, value] of Object.entries({
+          origin: globalRequestOptions?.origin,
+        })) {
+          if (value !== undefined) {
+            headers[`z-mitm-proxy-${key}`] = value;
+          }
+        }
+        return {
+          ...restProps,
+          headers,
+        };
+      },
+    });
+  };
+};
